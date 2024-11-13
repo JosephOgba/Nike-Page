db5d13da0f354b443282b8d3ee0e221b6c01601b
Joseph Ogba <josephogba247@gmail.com>
2024-11-13 18:44:32 +0000

    initial layout

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const App =() =>(
+ <main className="relative">
+ nav
+ <section className="xl:padding-1 wide:padding-r padding-b">
+  Hero
+ </section>
+ <section className="padding">
+  PopularProducts
+ </section>
+ <section className="padding">
+  SuperQuality 
+ </section> <section className="padding-x py-10">
+  Services 
+ </section> <section className="padding">
+  Specialoffer
+ </section>
+ <section className="bg-pale-blue padding">
+  CustomerReviews
+ </section><section className="padding-x sm:py-32 py-16 w-full">
+Suscribe
+ </section><section className="padding bg-black padding-x padding-t pb-8">
+Footer
+ </section>
+ </main>
+);
+export default App;
