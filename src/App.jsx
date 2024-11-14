57f5442e46bfed2762f098dcb9e9041f12ffc349
ogba Joseph <josephogba247@gmail.com>
2024-11-14 10:01:30 +0000

    Added Navigation bar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { Footer, Hero, CustomerReview, Suscribe, SuperQuality, SpecialOffer, PopularProduct,Services }  from './sections/index'
+import { Footer, Hero, CustomerReview, Suscribe, SuperQuality, SpecialOffer, PopularProduct,Services }  from './sections/index';
+import Nav from './components/Nav';
 
 const App =() =>(
  <main className="relative">
- nav
+ <Nav/>
  <section className="xl:padding-1 wide:padding-r padding-b">
-  Hero
+  <Hero/>
  </section>
  <section className="padding">
-  PopularProducts
+  <PopularProduct/>
  </section>
  <section className="padding">
-  SuperQuality 
+  <SuperQuality/>
  </section> <section className="padding-x py-10">
-  Services 
+  <Services/> 
  </section> <section className="padding">
-  Specialoffer
+  <SpecialOffer/>
  </section>
  <section className="bg-pale-blue padding">
-  CustomerReviews section coming soon 
+  <CustomerReview/> 
  </section><section className="padding-x sm:py-32 py-16 w-full">
-Suscribe
+<Suscribe/>
  </section><section className="padding bg-black padding-x padding-t pb-8">
-Footer Goes here 
+<Footer/> 
  </section>
  </main>
 );
