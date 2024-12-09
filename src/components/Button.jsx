3bf74ecee5cae5ddd8928378776f7141ddce1e60
ogba Joseph <josephogba247@gmail.com>
2024-12-09 16:18:30 +0000

    Added more sestions plus made it responsive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-const Button = ({label,iconURL}) => {
+const Button = ({label, iconURL}) => {
   return (
    <button className='flex justify-center items-center gap-2 bg-coral-red text-white-400 px-7 py-4 border font-montserrat text-lg leading-none rounded-full border-coral-red'>
      {label}
-     <img 
+    { iconURL && <img 
      className='ml-2 rounded-full w-5 h-5'
-     src={iconURL} alt="Right Arrow icon" />
+     src={iconURL} alt="Right Arrow icon" />}
    </button>
    
   )
